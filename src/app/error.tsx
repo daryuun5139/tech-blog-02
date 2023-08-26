5d9923004cfb76f32e020e51fc71e39ddd2b5b4e
“daryuun5139” <daryun6262@gmail.com>
2023-08-26 20:57:57 +0900

    add not-found.tsx & error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,1 +1,11 @@
 "use client";
+
+const Error = ({ error }: { error: Error }) => {
+  return (
+    <div className="md:pt-50 pt-40">
+      <p className="text-center">サーバーでのデータの取得に失敗しました。</p>
+    </div>
+  );
+};
+
+export default Error;
